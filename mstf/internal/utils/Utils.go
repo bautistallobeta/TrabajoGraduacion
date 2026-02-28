30c24c1bf40a36e85a383ff06499d923b4bc1a8d
bautistallobeta <llobetabautista@gmail.com>
2026-02-27 21:03:03 -0300

    feat: api documentada en swagger

--- mstf/internal/utils/Utils.go
+++ mstf/internal/utils/Utils.go
@@ -16,7 +16,6 @@ import (
 
 // Convierte dos uint64 en un string concatenado.
 // El 'low' se rellena con ceros a 20 dígitos para distinguirlo
-// TODO: EVALUAR REESCRIBIR SIN PADDING
 func ConcatenarIDString(high uint64, low uint64) string {
 	lowString := fmt.Sprintf("%020d", low)
 	highString := fmt.Sprintf("%d", high)
@@ -52,7 +51,6 @@ func Uint128AStringDecimal(id types.Uint128) string {
 }
 
 func FechaAUserData128(s string) (types.Uint128, error) {
-	//TODO: revisar formateo enedge cases
 	layouts := []string{
 		"2006-01-02 15:04:05.999999999",
 		"2006-01-02 15:04:05.999999",
@@ -111,7 +109,7 @@ func FechaAUserData32(s string) (uint32, error) {
 	}
 	segundos := t.Unix()
 
-	// Validar que entra en uint32 (hasta año 2106) - TODO: revisar si conviene cambiar esto
+	// Validar que entra en uint32 (hasta año 2106)
 	if segundos < 0 || segundos > 4294967295 {
 		return 0, errors.New("Fecha fuera de rango válido para uint32 (1970-2106)")
 	}
